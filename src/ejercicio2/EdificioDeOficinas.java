6c82289a15299ce3b19e446e2159fcb08a12b549
svanderstichel <vanderstichel@gmail.com>
2026-04-15 17:47:42 -0300

    Se implemento los getters de EdificioDeOficinas

--- src/ejercicio2/EdificioDeOficinas.java
+++ src/ejercicio2/EdificioDeOficinas.java
@@ -1,24 +1,26 @@
 package ejercicio2;
 
 public class EdificioDeOficinas implements Edificio {
-	private double superficie;
+	private double superficieOficina;
 	private int numeroDeOficinas;
 	
 	public EdificioDeOficinas() {
-		superficie = 0;
+		superficieOficina = 0;
 		numeroDeOficinas = 0;
 	}
 	
 	public EdificioDeOficinas(double superficie, int numeroDeOficinas) {
-		this.superficie = superficie;
+		this.superficieOficina = superficie;
 		this.numeroDeOficinas = numeroDeOficinas;
 	}
-
 	
+	public int getNumeroDeOficinas() {
+		return numeroDeOficinas;
+	}
+
 	@Override
 	public double getSuperficieEdificio() {
-		// TODO
-		return 0;
+		return superficieOficina*numeroDeOficinas;
 	}
 	
 }
