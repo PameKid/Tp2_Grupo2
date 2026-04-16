f6ec8bfd4acbf3de14f08afad1874ca1405bcc7d
SebastianDte <sebateunmate@outlook.com>
2026-04-16 04:39:42 -0300

    Agregué validación de null en verificarDNI

--- src/ejercicio1/MainEjercicio1_a.java
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -3,16 +3,21 @@ package ejercicio1;
 public class MainEjercicio1_a {
 
 	public static void main(String[] args) {
+        Persona personaDniInvalido = new Persona();
+        personaDniInvalido.setDni("AA202020");
+
+        Persona personaDniValido = new Persona();
+        personaDniValido.setDni("20202020");
 		
 		try {
-            Persona.verificarDNI("AA202020");
+                Persona.verificarDNI(personaDniInvalido.getDni());
             System.out.println("Persona agregada correctamente");
         } catch (ExVerificarDNI e) {
             System.out.println("Persona no agregada por no verificar el DNI");
         }
 		
 		try {
-            Persona.verificarDNI("20202020");
+                Persona.verificarDNI(personaDniValido.getDni());
             System.out.println("Persona agregada correctamente");
         } catch (ExVerificarDNI e) {
             System.out.println("Persona no agregada por no verificar el DNI");
